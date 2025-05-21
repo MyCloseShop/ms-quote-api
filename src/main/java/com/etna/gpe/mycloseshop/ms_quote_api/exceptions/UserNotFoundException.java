4e8cd37f0c0a15f569a3caf2d829e0ba276ab411
amara_s <amara_s@etna-alternance.net>
2025-05-21 10:38:43 +0200

    Initial commit

---
+++ src/main/java/com/etna/gpe/mycloseshop/ms_quote_api/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.etna.gpe.mycloseshop.ms_quote_api.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
